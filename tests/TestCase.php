a8c5a80cd3646e9b57f98947f589299f6dab6539
Bhone Wai <damenaosan@gmail.com>
2026-02-28 12:19:35 +0630

    test(ci): force test HTTP host to configured central domain for domain-scoped routes

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,16 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $centralDomain = config('tenancy.central_domains.0');
+
+        if (is_string($centralDomain) && $centralDomain !== '') {
+            $this->withServerVariables([
+                'HTTP_HOST' => $centralDomain,
+            ]);
+        }
+    }
 }
