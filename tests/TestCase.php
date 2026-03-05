ce5c555406ebcacd18056b549baafdf8e148ac9d
Bhone Wai <damenaosan@gmail.com>
2026-03-05 14:58:22 +0630

    test(e2e): add tenancy end-to-end flow regression coverage

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -9,6 +9,7 @@ abstract class TestCase extends BaseTestCase
     protected function setUp(): void
     {
         parent::setUp();
+        $this->withoutVite();
 
         $centralDomain = config('tenancy.central_domains.0')
             ?: parse_url((string) config('app.url', 'http://localhost'), PHP_URL_HOST)
